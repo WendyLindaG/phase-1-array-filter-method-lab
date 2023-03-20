d2a6f1d5b8c9600e313948327167e85856b23635
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-03-20 13:27:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+  
+
+
+function findMatching(drivers, name) {
+ return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+ if (name=="Susan") {
+    return [];
+}
+    
+
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
